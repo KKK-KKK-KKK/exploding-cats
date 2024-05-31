c7f388a3aae8fd008888a45c3bf92fa488e90b03
KKK-KKK-KKK <borek.jakub@gmail.com>
2024-05-31 17:36:42 +0200

    Code update
    Optimized drawing and added new card mechanics

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import time
 from menu import Menu
 from button import Button
 from cardtypes import *
@@ -76,11 +77,12 @@ class Game:
         self.player_two.hand.reveal_hand()
 
     def draw_player_two_pass_button(self, surface):
-        self.player_two_pass_button = Button(self.pass_image_path, (500,500))
+        if self.player_two_pass_button is None:
+            self.player_two_pass_button = Button(self.pass_image_path, (500,500))
         self.player_two_pass_button.draw_button(surface)
 
     def draw(self, screen):
-        screen.blit(self.background_image, (0, 0))  # Rysowanie tła jako pierwsze
+        screen.blit(self.background_image, (0, 0))
         if self.deck:
             self.deck.draw(screen)
         if self.player_one and self.player_two:
@@ -99,33 +101,94 @@ class Game:
                 pass #Defuse
             elif card_type == 2:
                 print("TacoCat action executing")
+                if len(self.stack.cards) > 1 and self.stack.cards[-2].card_type == 2:
+                    print("Stealing card")
+                    stolen_card = player_target.hand.pop_from_hand()
+                    player_user.hand.push_to_hand(stolen_card)
             elif card_type == 3:
                 print("RainbowCat action executing")
+                if len(self.stack.cards) > 1 and self.stack.cards[-2].card_type == 3:
+                    print("Stealing card")
+                    stolen_card = player_target.hand.pop_from_hand()
+                    player_user.hand.push_to_hand(stolen_card)
             elif card_type == 4:
                 print("BeardCat action executing")
+                if len(self.stack.cards) > 1 and self.stack.cards[-2].card_type == 4:
+                    print("Stealing card")
+                    stolen_card = player_target.hand.pop_from_hand()
+                    player_user.hand.push_to_hand(stolen_card)
             elif card_type == 5:
                 print("Favor action executing")
                 stolen_card = player_target.hand.pop_from_hand()
-                player_user.hand.push_to_hand(stolen_card)
+                player_user.hand.push_to_hand(stolen_card) 
+            elif card_type == 6:
+                print("Skip action executing")
+                player_user.skip = True
+            elif card_type == 7:
+                print("Reveal action executing")
+                self.deck.cards[-1].reveal_card((350,300))
+                if len(self.deck.cards) > 1:
+                    self.deck.cards[-2].reveal_card((420,300))
+                if len(self.deck.cards) > 2:
+                    self.deck.cards[-3].reveal_card((490,300))
+            elif card_type == 8:
+                print("Attack action executing")
+                player_user.skip = True
+                player_target.double_throw = True
+                #Skip turn. Enemy player has to play cards twice.
+                return 8
+            elif card_type == 9:
+                print("Shuffle action executing")
+                print(self.deck.cards)
+                random.shuffle(self.deck.cards)
+                print(self.deck.cards)
+                pass
+            elif card_type == 10:
+                print("Nope action executing")
+                pass
+
+            return False
         
 
 
-
-        popped_card = self.player_one.hand.update()
-        if popped_card is not None:
-            self.stack.update(popped_card)
-            do_card_action(self.player_one, self.player_two, popped_card.card_type) #act upon the type of the card
-        passed = self.player_one.hand.pass_turn(self.deck)
-
+        #Hotseat version
+        # popped_card = self.player_one.hand.update()
+        # if popped_card is not None:
+        #     self.stack.update(popped_card)
+        #     do_card_action(self.player_one, self.player_two, popped_card.card_type) #act upon the type of the card
+           
         popped_card = self.player_two.hand.update()
+        card_action = None
         if popped_card is not None:
             self.stack.update(popped_card)
-            do_card_action(self.player_two, self.player_one, popped_card.card_type) #act upon the type of the card
-        passed = self.player_two.hand.pass_turn(self.deck)
+            card_action = do_card_action(self.player_two, self.player_one, popped_card.card_type) #act upon the type of the card
+            self.player_two.hand.reveal_hand()
+            self.player_one.hand.hide_hand()
+        passed = self.player_two_pass_button.update()
+        if passed:
+            if self.player_two.skip == False:
+                self.player_two.hand.pass_turn(self.deck)
+            self.player_two.hand.reveal_hand()
+            #AI player turn
+            popped_card = self.player_one.AI_turn()
+            self.stack.update(popped_card)
+            do_card_action(self.player_one, self.player_two, popped_card.card_type)
+            if self.player_one.double_throw == True:
+                popped_card = self.player_one.AI_turn()
+                self.stack.update(popped_card)
+                do_card_action(self.player_one, self.player_two, popped_card.card_type)
+
+            if self.player_one.skip == False:
+                self.player_one.hand.pass_turn(self.deck)
+            self.player_one.skip = False
+            self.player_one.double_throw = False
+            self.player_two.skip = False
+            self.player_two.double_throw = False
+            self.player_one.hand.hide_hand()
 
 
 class Deck:
-    card_types = [0, 1, 2, 3, 4, 5]  # always add enum when a new card is added to deck
+    card_types = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # always add enum when a new card is added to deck
     __MAX_EXPLOSIVE = 4
     __MAX_DEFUSE = 6
 
@@ -138,6 +201,7 @@ class Deck:
     def fill_deck(self, amount):
         def get_card_type(card_type, center_coords):
             card_version = random.choice([0, 1, 2])
+            card_version_lesser = random.choice([0,1])
 
             if card_type == 0:
                 return ExplodingCat(center_coords)
@@ -161,6 +225,37 @@ class Deck:
                     return Favor_1(center_coords)
                 if card_version == 2:
                     return Favor_2(center_coords)
+            elif card_type == 6:
+                if card_version == 0:
+                    return Skip(center_coords)
+                if card_version == 1:
+                    return Skip_1(center_coords)
+                if card_version == 2:
+                    return Skip_2(center_coords)
+            elif card_type == 7:
+                if card_version == 0:
+                    return Reveal(center_coords)
+                if card_version == 1:
+                    return Reveal_1(center_coords)
+                if card_version == 2:
+                    return Reveal_2(center_coords)
+            elif card_type == 8:
+                if card_version_lesser == 0:
+                    return Attack(center_coords)
+                if card_version_lesser == 1:
+                    return Attack_1(center_coords)
+            elif card_type == 9:
+                if card_version_lesser == 0:
+                    return Shuffle(center_coords)
+                if card_version_lesser == 1:
+                    return Shuffle_1(center_coords)
+            elif card_type == 10:
+                if card_version == 0:
+                    return Nope(center_coords)
+                if card_version == 1:
+                    return Nope_1(center_coords)
+                if card_version == 2:
+                    return Nope_2(center_coords)
             else:
                 try:
                     raise Exception("Incorrect card type selected in deck", "Exiting game")
@@ -206,9 +301,17 @@ class Deck:
         return viable_card
 
     def draw(self, surface):
-        for i in range(len(self.cards)):
-            self.cards[i].load_image((300, 400))
-            self.cards[i].draw(surface)
+        last_cards = []
+        if len(self.cards) > 2:
+            last_cards = [self.cards[-1], self.cards[-2], self.cards[-3]]
+        elif len(self.cards) == 2:
+            last_cards = [self.cards[-1], self.cards[-2]]
+        elif len(self.cards) == 1:
+            last_cards = [self.cards[-1]]
+
+        for i in range(0, len(last_cards)):
+            last_cards[i].load_image((300, 400))
+            last_cards[i].draw(surface)
         self.visibility = True
 
     def __check_type_no_explosion_no_defuse(self, card):
@@ -245,10 +348,11 @@ class Stack:
         print("Stack created")
 
     def draw(self, surface):
-        for i in range(len(self.cards)):
-            self.cards[i].load_image(self.stack_position)
-            self.cards[i].place_card(self.stack_position)
-            self.cards[i].draw(surface)
+        stack_cards_size = len(self.cards) 
+        if stack_cards_size > 0:
+            self.cards[stack_cards_size-1].load_image(self.stack_position)
+            self.cards[stack_cards_size-1].place_card(self.stack_position)
+            self.cards[stack_cards_size-1].draw(surface)
 
     def update(self, popped_card):
         self.cards.append(popped_card)
@@ -260,7 +364,14 @@ class Player:
     def __init__(self, nickname):
         self.score = 0
         self.nickname = nickname
+        self.double_throw = False
+        self.skip = False
         self.hand = Hand()
+
+    def AI_turn(self):
+        time.sleep(0.4)
+        popped_card = self.hand.update(True) #could be changed to "cards" and accept array
+        return popped_card
 
     def update(self):
         pass
@@ -297,11 +408,15 @@ class Hand:
         self.cards.append(card)
 
     def pass_turn(self, deck):
-        pass
+        self.cards.append(deck.draw_card())
 
     def reveal_hand(self):
         for i in range(0, len(self.cards)):
             self.cards[i].obverse = True
+
+    def hide_hand(self):
+        for i in range(0, len(self.cards)):
+            self.cards[i].obverse = False
 
     def print_hand(self):
         cards = [card.card_type for card in self.cards]
@@ -312,7 +427,7 @@ class Hand:
             offset_y = 130
         elif side == "bottom":
             offset_y = 650
-        for i in range(len(self.cards)):
+        for i in range(len(self.cards)): #TODO: popraw ułożenie kart
             if i == 10 and side == "bottom":
                 offset_y += 30
             if i == 10 and side == "top":
@@ -322,14 +437,22 @@ class Hand:
             self.cards[i].draw(surface)
 
     
-    def update(self):
+    def update(self, ai_turn=False):
         top_index = -1
+        if ai_turn:
+            top_index = random.randrange(len(self.cards)-1)
+            self.cards[top_index].update(self.width_offset, True)
+            print(self.cards)
+            print(f"Hand cards length: {len(self.cards)-1}")
+            print(f"top_index: {top_index}")
+            return self.cards.pop(top_index)
+
         for i in range(0, len(self.cards)):
             put_card_on_stack = self.cards[i].update(self.width_offset)
             if put_card_on_stack:
                 top_index = i
 
-        if top_index is not -1:
+        if top_index != -1:
             print(self.cards)
             print(f"Hand cards length: {len(self.cards)-1}")
             print(f"top_index: {top_index}")
@@ -385,7 +508,6 @@ def main():
                 game.reveal_player_two_cards()
                 
         else:
-            print('test')
             game.draw(screen)
             game.update()
         pygame.display.update()
